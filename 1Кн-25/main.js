19fe8577ffd3970ec901acfc6e606dc259f2e98a
Samuarailx <s.lukianenko2020@gmail.com>
2026-05-06 18:30:32 +0300

    Add files via upload

---
+++ 1Кн-25/main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const trailerModal = document.getElementById('trailerModal');
+    const videoIframe = document.getElementById('trailerVideo');
+
+    if (trailerModal) {
+        trailerModal.addEventListener('show.bs.modal', function (event) {
+            const button = event.relatedTarget;
+            let videoUrl = button.getAttribute('data-video');
+            if (videoUrl.includes('watch?v=')) {
+                videoUrl = videoUrl.replace('watch?v=', 'embed/');
+            }
+            videoIframe.setAttribute('src', videoUrl + "?autoplay=1");
+        });
+
+        trailerModal.addEventListener('hidden.bs.modal', function () {
+            videoIframe.setAttribute('src', '');
+        });
+    }
+});
